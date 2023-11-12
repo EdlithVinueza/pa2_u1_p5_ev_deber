153202638bb58be11f8c93b7f27f923aa9cb03b0
Edlith Vinueza <eavinuezaz1@uce.edu.ec>
2023-11-11 20:13:47 -0500

    Subir proyecto por primera vez

---
+++ src/main/java/com/uce/edu/Pa2U1P5EvDeberApplication.java
@@ -0,0 +1,23 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class Pa2U1P5EvDeberApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5EvDeberApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Deber de Programación Avanzada 2");
+		System.out.println("Tema: Uso de GitHub");
+		System.out.println("Nombre: Edlith Vinueza");
+		System.out.println("Subir proyecto por primera vez");
+		
+	}
+
+}
